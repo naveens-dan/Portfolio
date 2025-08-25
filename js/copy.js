a35d19d43db25d5a4915ccc4a3b606ac5d5c3bda
naveens <naveens.dan@gmail.com>
2025-08-25 13:05:53 +0530

    index

--- js/copy.js
+++
@@ -1,8 +0,0 @@
-class copiedSuccess extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-<div id="copied-success" class="copied">Copied</div>
-        `
-    }
-}
-window.customElements.define('dan-copied', copiedSuccess)
